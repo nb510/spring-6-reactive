cd0406bfbf48b43d9e6aaa6ccb069fe57ae89014
nburak <nburak@fnc-group.ru>
2025-07-20 14:26:36 +0300

    add relational database

---
+++ src/main/java/guru/springframework/spring6reactive/bootstrap/BootstrapData.java
@@ -0,0 +1,65 @@
+package guru.springframework.spring6reactive.bootstrap;
+
+import guru.springframework.spring6reactive.domain.Beer;
+import guru.springframework.spring6reactive.repository.BeerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Component
+public class BootstrapData implements CommandLineRunner {
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        loadData();
+        beerRepository.count().subscribe(count -> System.out.printf("Found %s beers%n", count));
+    }
+
+    private void loadData() {
+        beerRepository.count().subscribe(count -> {
+            if (count != 0) {
+                return;
+            }
+
+            Beer beer1 = Beer.builder()
+                    .beerName("Galaxy Cat")
+                    .beerStyle("Pale Ale")
+                    .upc("12356")
+                    .price(new BigDecimal("12.99"))
+                    .quantityOnHand(122)
+                    .createdDate(LocalDateTime.now())
+                    .lastModifiedDate(LocalDateTime.now())
+                    .build();
+
+            Beer beer2 = Beer.builder()
+                    .beerName("Crank")
+                    .beerStyle("Pale Ale")
+                    .upc("12356222")
+                    .price(new BigDecimal("11.99"))
+                    .quantityOnHand(392)
+                    .createdDate(LocalDateTime.now())
+                    .lastModifiedDate(LocalDateTime.now())
+                    .build();
+
+            Beer beer3 = Beer.builder()
+                    .beerName("Sunshine City")
+                    .beerStyle("IPA")
+                    .upc("12356")
+                    .price(new BigDecimal("13.99"))
+                    .quantityOnHand(144)
+                    .createdDate(LocalDateTime.now())
+                    .lastModifiedDate(LocalDateTime.now())
+                    .build();
+
+            beerRepository.save(beer1).subscribe();
+            beerRepository.save(beer2).subscribe();
+            beerRepository.save(beer3).subscribe();
+        });
+    }
+}
